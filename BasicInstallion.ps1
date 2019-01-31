68ac4a2621c1ab664c8d8b77e34ed6e13e7ac91c
multiplies support <support@multiplies.be>
2019-01-31 11:26:07 +0100

    adding adobe reader to auto install and checks if it completes succesfully

--- BasicInstallion.ps1
+++ BasicInstallion.ps1
@@ -1,6 +1,28 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
+function installProcess(){
+    if($(Get-Process -Name Ninite -ErrorAction SilentlyContinue -ErrorVariable errorNinite) -or $(Get-Process -Name readerdc_nl_xa_crd_install -ErrorAction SilentlyContinue -ErrorVariable errorReaderDC)){
+        return $true
+    }else{
+        $events = (Get-EventLog -LogName Application -EntryType Information -InstanceId 11707 -Source MsiInstaller -After $time -ErrorAction SilentlyContinue| where {$_.Message -like "*Adobe Acrobat Reader DC*"} -ErrorAction SilentlyContinue)
+        if($events){
+            installAdobeReaderDC
+        }else{
+            return $false
+        }
+    }
+}
+function installAdobeReaderDC(){
+    Start-Process -FilePath "$dir\readerdc_nl_xa_crd_install.exe" -Verb runAs
+}
+
 $dir = [Environment]::GetFolderPath("MyDocuments")
+if(!$(Test-Path -Path $dir/temp)){
+    mkdir "$dir\temp"
+}
+$dir = "$dir\temp"
+
+$time = get-date
 
 $date = (Get-Date -Format o).split(".")[0].Replace(":",".")
 wget "https://ninite.com/.net4.7.2-7zip-air-chrome-java8-shockwave-silverlight-teamviewer/ninite.exe" -OutFile "$dir\ninite_$date.exe"
@@ -9,21 +31,28 @@ Start-Process -FilePath "$dir\ninite_$date.exe" -Verb runAs
 
 Set-ItemProperty -Path 'Registry::HKU\.DEFAULT\Control Panel\Keyboard' -Name "InitialKeyboardIndicators" -Value "2147483650"
 
-get-appxpackage *candy* | remove-appxpackage
-get-appxpackage *fitbit* | remove-appxpackage
-get-appxpackage *Microsoft.Office.Desktop* | remove-appxpackage
-Get-AppxPackage *news* | remove-appxpackage
-Get-AppxPAckage *C27EB4BA.DropboxOEM* | remove-appxpackage
-Get-AppxPackage *Amazon.com.Amazon* | remove-appxpackage
-Get-AppxPackage *5A894077.McAfeeSecurity* | remove-appxpackage
-Get-AppxPackage *MicrosoftSolitaireCollection* | Remove-AppxPackage
-Get-AppxPackage *ThumbmunkeysLtd.PhototasticCollage* | Remove-AppxPackage
+wget "https://raw.githubusercontent.com/multiplies/uwp_remove_list/master/uwp.txt" -OutFile "$dir\uwp.txt"
 
-Start-Sleep -Seconds 25
+$uwp = Get-Content "$dir\uwp.txt"
+$i = 1 
+$uwp | foreach {
+    $lines = $uwp.Length
+    Write-Progress -Activity "Removing UWP Apps" -status "Removing app $_" -percentComplete ($i / $lines*100)
+    get-appxpackage $_ | remove-appxpackage
+    $i++
+}
 
-$url = 'https://get.adobe.com/nl/reader'
+$url = 'https://admdownload.adobe.com/bin/live/readerdc_nl_xa_crd_install.exe'
+wget $url -OutFile "$dir\readerdc_nl_xa_crd_install.exe"
+installAdobeReaderDC
 
-(New-Object -Com Shell.Application).Open($url)
+Start-Sleep -Seconds 60
+Write-host "Waiting"
+do{
+    Write-Host '.' -NoNewline -ForegroundColor Magenta
+    Start-Sleep -Seconds 10
+}while(installProcess)
 
-Wait-Process -Name Ninite
-rm "$dir\ninite_$date.exe" -force
+
+
+remove-item -Path $dir -Force -Recurse
